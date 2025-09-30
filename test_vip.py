708ccf847e650a7bac34240226ffed6ae8e61830
김준회(mrt1847) <mrt1847@gepplus.net>
2025-09-30 10:40:42 +0900

    Update test_vip.py

--- test_vip.py
+++ test_vip.py
@@ -10,28 +10,33 @@ from vip_data import vip_testcases1, vip_testcases2
 
 @pytest.mark.flaky(reruns=2, reruns_delay=1)
 @pytest.mark.parametrize("keyword, case_id", vip_testcases1, ids=[c for _, c in vip_testcases1])
-def test_vip_1(page):
+def test_vip_1(page, keyword, case_id, request):
+    request.node._testrail_case_id = case_id
     etc = Etc(page)
     vip_page = Vip(page)
-    db_check = DatabricksSPClient()
+    logger = TimeLogger("test_vip.json")
     # vip 로 이동
     etc.goto_vip()
 
     # VT 모듈로 이동 후 확인
+    logger.record_time("case1", keyword, "exposure")
     parent = vip_page.vip_module_by_title("함께 보면 좋은 상품이에요")
     # 광고상품 상품 번호 추출
     result = vip_page.assert_item_in_module("함께 보면 좋은 상품이에요")
     goodscode = result["goodscode"]
     target = result["target"]
     # 상품 클릭후 해당 vip 이동 확인
+    logger.record_time("case1", keyword, "click")
     vip_page.click_goods(goodscode, target)
 
 @pytest.mark.flaky(reruns=2, reruns_delay=1)
-@pytest.mark.parametrize("keyword, case_id", search_testcases2, ids=[c for _, c in search_testcases2])
-def test_vip_2(page):
+@pytest.mark.parametrize("keyword, case_id", vip_testcases2, ids=[c for _, c in vip_testcases2])
+def test_vip_2(page, keyword, case_id, request):
+    request.node._testrail_case_id = case_id
+    logger = TimeLogger("test_vip.json")
     etc = Etc(page)
     vip_page = Vip(page)
-    db_check = DatabricksSPClient()
+
     # vip 로 이동
     etc.goto_vip_starship()
 
